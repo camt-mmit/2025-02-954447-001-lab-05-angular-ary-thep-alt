b9e5a3a8e07fa9c124d9e633abad114ec8c10ebc
John Doe <thepkhamlueariya@gmail.com>
2025-12-15 16:39:46 +0700

    hii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HelloWorld } from './hello-world/hello-world';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+
+    redirectTo: 'hello-world',
+    pathMatch: 'full',
+  }, // default route
+  { path: 'hello-world', component: HelloWorld },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('week-05');
